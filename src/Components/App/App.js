2cb923a6b1e10f8d5d715eb62a97384c5d2d2d9c
John Kiernan <jkiernan12@yahoo.com>
2021-12-15 11:25:21 -0500

    Fetch and display basic months and staff

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,19 +1,40 @@
 import './App.css';
+import { Component } from 'react';
 import { months } from '../../months_data';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      months,
+      staff: []
+    }
+  }
 
-  return (
-    <div className="App">
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/birthdays')
+    .then(res => res.json())
+    .then(data => this.setState({staff: data}))
+  }
+
+  render() {
+    return (
+      <div className="App">
       <h1>Birthdays</h1>
       <div className='bday-form'>
 
       </div>
       <div className='bday-container'>
-
+      {this.state.months.map(month => {
+        return <h2 key={month.id}>{month.name}</h2>
+      })}
+      {this.state.staff.map(person => {
+        return <h3 key={person.id}>{person.name}</h3>
+      })}
       </div>
     </div>
   );
+}
 }
 
 export default App;
