06fcb3a4e805f6306e01a07a7f24c2a1003f4468
John Kiernan <jkiernan12@yahoo.com>
2021-12-15 12:42:26 -0500

    Fix values not clearing bug

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -32,11 +32,11 @@ class Form extends Component {
     return (
     <form>
       <label htmlFor='nameValue'>Name:</label>
-      <input id='nameValue' type='text' onChange={this.updateValue}/>
+      <input id='nameValue' type='text' value={this.state.nameValue} onChange={this.updateValue}/>
       <label htmlFor='monthValue' >Month:</label>
-      <input id='monthValue' type='number' onChange={this.updateValue} />
+      <input id='monthValue' type='number' value={this.state.monthValue} onChange={this.updateValue} />
       <label htmlFor='dayValue'>Day:</label>
-      <input id='dayValue' type='number' onChange={this.updateValue} />
+      <input id='dayValue' type='number' value={this.state.dayValue} onChange={this.updateValue} />
       <button type="submit" onClick={this.createBirthday}>Add this birthday!</button>
     </form>
     )
